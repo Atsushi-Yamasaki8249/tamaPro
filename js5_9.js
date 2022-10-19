2f01499f31907277945ad1398239d1dbb8f9bec8
Atsushi-Yamasaki8249 <106168889+Atsushi-Yamasaki8249@users.noreply.github.com>
2022-10-19 23:25:00 +0900

    Add files via upload

---
+++ js5_9.js
@@ -0,0 +1,15 @@
+let a = 4;
+let b = 5;
+let n1 = 6;
+let n2 = 7;
+
+display_multiply(n1, n2);
+
+console.log(a, b, n1, n2);
+
+function display_multiply(n1, n2){
+    let a = 8;
+    b = 10;
+    console.log(n1*n2);
+    n1 = 12;
+}
