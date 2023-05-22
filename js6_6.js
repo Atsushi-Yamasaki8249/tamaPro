b92a464c646dc481992a1e67757219cb83a1a736
Atsushi-Yamasaki8249 <106168889+Atsushi-Yamasaki8249@users.noreply.github.com>
2023-05-22 21:46:21 +0900

    Delete js6_6.js

--- js6_6.js
+++
@@ -1,7 +0,0 @@
-let list = document.getElementsByTagName('td');
-
-for(let i=0; i < list.length; i++){
-    list[i].addEventListener('mouseenter', function(){
-        this.textContent = "わお！";
-    }, false);
-}
