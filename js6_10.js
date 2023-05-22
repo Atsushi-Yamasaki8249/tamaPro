8c59d0c8756bf87727440c7c725a6c55e6ab980a
Atsushi-Yamasaki8249 <106168889+Atsushi-Yamasaki8249@users.noreply.github.com>
2023-05-22 21:44:17 +0900

    Delete js6_10.js

--- js6_10.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    let random = Math.random();
-    console.log(random);
-    if(random >= 0.5){
-        let elem = $("<p></p>")
-        elem.text("50%の確率でこのテキストが表示されます");
-        $("#contents").append(elem);
-    }
-});
