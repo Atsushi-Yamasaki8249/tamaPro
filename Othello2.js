e8173c233cf91e67fb0f4ecbfbf9f3d85edc5168
Atsushi-Yamasaki8249 <106168889+Atsushi-Yamasaki8249@users.noreply.github.com>
2023-05-22 20:48:03 +0900

    Delete Othello2.js

--- Othello2.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('#sqa3_3').text('●').addClass('white');
-    $('#sqa3_4').text('●').addClass('black');
-    $('#sqa4_3').text('●').addClass('black');
-    $('#sqa4_4').text('●').addClass('white');
-});
